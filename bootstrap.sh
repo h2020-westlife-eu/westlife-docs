3237bbd3f49d1f0bf5f97aa9fca58c02d89e6e86
TomasKulhanek <tomatonek12345>
2018-05-18 11:41:52 +0100

    UPDATE: scripts for rendering pdf

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#git clone https://github.com/h2020-westlife-eu/westlife-docs.git
+#cd westlife-docs/
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+sudo npm install gitbook-cli -g
+gitbook install
+gitbook pdf ./ ./westlifedocs.pdf
